f4e146bf72a2c75fa3b9f4977f9c02e6ed4ec501
josiahelmer <jelm9149@740s-pl-112.local>
2016-01-14 13:55:09 -0700

    text

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,18 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: jelm9149
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is how you talk to the console in C++" << endl;
+	//This is like System.out.printl("Some text"); in java
+
+	return 0;
+}
+
